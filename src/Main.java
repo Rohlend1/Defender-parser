973951f6e6401234d7cae9e63a68e1cccefabe92
Rohlend1 <gskosh1w@gmail.com>
2024-06-12 20:59:16 +0300

    fix bugs

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        //TIP <b>Указать свою директорию где лог файл</b>
-        new FileListener("<root_dir>");
+        //TIP <b>Указать свою директорию где лог файл.
+        // В формате '/Users/JohnDoe/root_directory/'.
+        // В конце '/' обязателен</b>
+        new FileListener("<root_directory>");
     }
 }
